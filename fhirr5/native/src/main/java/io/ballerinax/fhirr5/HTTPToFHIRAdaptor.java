e2ff4857c2fe81ba562acbc12b830697f942640a
akila94 <akilaaroshanaa@gmail.com>
2025-06-12 14:31:25 +0530

    Add fhirr5 implementation
    Rename package names to r5
    
    Update version and remove remove r5 and international500 changes to keep fhirr5 changes only in the PR
    
    Change version in readme files
    
    Add fhirr5 workflow support
    
    Format fhirr5 code
    
    Add missing end lines to pom files
    
    Update fhirr5/ballerina/src/main/resources/fhirservice/Ballerina.toml
    
    Co-authored-by: sameeragunarathne <sameera910409@gmail.com>
    
    Update fhirr5/compiler-plugin/src/main/java/io/ballerinax/fhirr5/compiler/Constants.java
    
    Co-authored-by: sameeragunarathne <sameera910409@gmail.com>
    
    Update fhirr5/compiler-plugin/src/main/java/io/ballerinax/fhirr5/compiler/FHIRServiceValidator.java
    
    Co-authored-by: sameeragunarathne <sameera910409@gmail.com>
    
    Add FHIRR5 services
    
    Fix PR comments

---
+++ fhirr5/native/src/main/java/io/ballerinax/fhirr5/HTTPToFHIRAdaptor.java
@@ -0,0 +1,135 @@
+/*
+ * Copyright (c) 2025, WSO2 LLC. (http://www.wso2.org).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io.ballerinax.fhirr5;
+
+import io.ballerina.runtime.api.Environment;
+import io.ballerina.runtime.api.concurrent.StrandMetadata;
+import io.ballerina.runtime.api.types.ResourceMethodType;
+import io.ballerina.runtime.api.types.ServiceType;
+import io.ballerina.runtime.api.values.BArray;
+import io.ballerina.runtime.api.values.BMap;
+import io.ballerina.runtime.api.values.BObject;
+import io.ballerina.runtime.api.values.BString;
+
+import static io.ballerinax.fhirr5.Utils.isPathsMatching;
+
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Class responsible for mapping a FHIR service to its underlying HTTP service
+ */
+public class HTTPToFHIRAdaptor {
+
+    private static final String MODULE_NAME = "ballerinax/health.fhirr5";
+
+    public static final String PATH_PARAM_IDENTIFIER = "^";
+    // Special escape character used to escape the special characters in the path parameter
+    public static final String SPECIAL_ESCAPE_CHAR = "\\";
+
+    private HTTPToFHIRAdaptor(){}
+
+    public static void addFhirServiceToHolder(BObject holder, BObject service) {
+        holder.addNativeData("FHIR_SERVICE", service);
+    }
+
+    public static BObject getFhirServiceFromHolder(BObject holder) {
+        return (BObject) holder.getNativeData("FHIR_SERVICE");
+    }
+
+    public static Object executeWithID(Environment environment, BString id, BObject fhirCtx, BObject service,
+                                              ResourceMethodType resourceMethod) {
+        ServiceType serviceType = (ServiceType) service.getType();
+        return environment.getRuntime().callMethod(service, resourceMethod.getName(),
+                new StrandMetadata(serviceType.isIsolated() && serviceType.isIsolated(resourceMethod.getName()), ModuleUtils.getProperties("executeWithID")), id, fhirCtx);
+    }
+
+    public static Object executeWithIDAndVID(Environment environment, BString id, BString vid, BObject fhirCtx,
+                                             BObject service, ResourceMethodType resourceMethod) {
+        ServiceType serviceType = (ServiceType) service.getType();
+        return environment.getRuntime().callMethod(service, resourceMethod.getName(),
+                new StrandMetadata(serviceType.isIsolated() && serviceType.isIsolated(resourceMethod.getName()), ModuleUtils.getProperties("executeWithIDAndVID")), id, vid, fhirCtx);
+    }
+
+    public static Object executeWithNoParam(Environment environment, BObject fhirCtx, BObject service,
+                                         ResourceMethodType resourceMethod) {
+        ServiceType serviceType = (ServiceType) service.getType();
+        return environment.getRuntime().callMethod(service, resourceMethod.getName(),
+                new StrandMetadata(serviceType.isIsolated() && serviceType.isIsolated(resourceMethod.getName()), ModuleUtils.getProperties("executeWithNoParam")), fhirCtx);
+    }
+
+    public static Object executeWithPayload(Environment environment, BMap r5Payload, BObject fhirCtx, BObject service,
+                                       ResourceMethodType resourceMethod) {
+        ServiceType serviceType = (ServiceType) service.getType();
+        return environment.getRuntime().callMethod(service, resourceMethod.getName(),
+                new StrandMetadata(serviceType.isIsolated() && serviceType.isIsolated(resourceMethod.getName()), ModuleUtils.getProperties("executeWithPayload")), fhirCtx, r5Payload);
+    }
+
+    public static Object executeWithIDAndPayload(Environment environment, BString id, BMap patchPayload,
+                                                 BObject fhirCtx, BObject service, ResourceMethodType resourceMethod) {
+        ServiceType serviceType = (ServiceType) service.getType();
+        return environment.getRuntime().callMethod(service, resourceMethod.getName(), new StrandMetadata(
+                serviceType.isIsolated() && serviceType.isIsolated(resourceMethod.getName()), ModuleUtils.getProperties("executeWithIDAndPayload")), id, fhirCtx, patchPayload);
+    }
+
+    public static Object getResourceMethod(BObject service, BArray servicePath, BArray path, BString accessor) {
+        ServiceType serviceType = (ServiceType) service.getType();
+        return getResourceMethod(serviceType, servicePath.getStringArray(), path.getStringArray(), accessor.getValue());
+    }
+
+    public static Object isHavingPathParam(ResourceMethodType resourceMethod) { 
+        String[] paths = resourceMethod.getResourcePath();
+        for (String s : paths) {
+            if (PATH_PARAM_IDENTIFIER.equals(s)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    private static ResourceMethodType getResourceMethod(ServiceType serviceType, String[] servicePath, String[] path, String accessor) {        
+        String isDebugEnabled = System.getenv("BAL_FHIR_SERVICE_DEBUG_ENABLED");
+        if (isDebugEnabled != null && isDebugEnabled.equals("true")) {
+            logDebug("Entering getResourceMethod with accessor: " + accessor +
+                 ", servicePath: " + String.join("/", servicePath) +
+                 ", path: " + String.join("/", path));
+        }
+        for (ResourceMethodType resourceMethod : serviceType.getResourceMethods()) {
+            if (isDebugEnabled != null && isDebugEnabled.equals("true")) {
+                logDebug("Resource method accessor: " + resourceMethod.getAccessor() +
+                     " with resource path: " + String.join("/", resourceMethod.getResourcePath()) + ", and request path: " + String.join("/",path));
+            }
+            if (accessor.equalsIgnoreCase(resourceMethod.getAccessor()) && isPathsMatching(resourceMethod.getResourcePath(), servicePath, path)) {
+                return resourceMethod;
+            }
+        }
+        return null;
+    }
+
+    private static String getCurrentTimestamp() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+        return OffsetDateTime.now().format(formatter);
+    }
+
+    private static void logDebug(String message) {
+        System.err.println("time=" + getCurrentTimestamp() + " level=DEBUG" + 
+                           " module=" + MODULE_NAME + " message=\"" + message + "\"");
+    }
+
+}
---
+++ fhirr5/native/src/main/java/io/ballerinax/fhirr5/Utils.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2025, WSO2 LLC. (http://www.wso2.org).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io.ballerinax.fhirr5;
+
+import static io.ballerinax.fhirr5.HTTPToFHIRAdaptor.PATH_PARAM_IDENTIFIER;
+import static io.ballerinax.fhirr5.HTTPToFHIRAdaptor.SPECIAL_ESCAPE_CHAR;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class Utils {
+
+    /**
+     * Checks if the length of the resource path is equal to the size of the request path.
+     *
+     * @param resourcePaths Ballerina service's resource method path
+     * @param requestPaths the list of paths of the request
+     * @return {@code true} if the length of the resource path array is equal to the size of the provided list of paths,
+     *         {@code false} otherwise
+     */
+    static boolean isPathsMatching(String[] resourcePaths, String[] servicePath, String[] requestPaths) {
+
+        if (resourcePaths[0].equals(".") && (servicePath.length == requestPaths.length)) {
+            return true;
+        }
+
+        if (servicePath.length == 0 && resourcePaths.length == requestPaths.length) {
+            int count = requestPaths.length - 1;
+            for (int i = resourcePaths.length - 1; i >= 0; i--) {
+                if (requestPaths[count].equals(resourcePaths[i]) || (resourcePaths[i].equals(PATH_PARAM_IDENTIFIER))) {
+                    if (resourcePaths[i].equals(PATH_PARAM_IDENTIFIER) && requestPaths[count].equals("_history")) {
+                        break;
+                    }
+                    count--;
+                    if (i == 0) {
+                        return true;
+                    }
+                } else {
+                    break;
+                }
+            }
+        }
+
+        resourcePaths = Stream.concat(Arrays.stream(servicePath), Arrays.stream(resourcePaths)).toArray(String[]::new);
+
+        if (resourcePaths.length != requestPaths.length || requestPaths.length == 0) {
+            return false;
+        }
+        for (int i = 0; i < resourcePaths.length; i++) {
+            String value1 = resourcePaths[i].contains(SPECIAL_ESCAPE_CHAR) ? resourcePaths[i].replace(SPECIAL_ESCAPE_CHAR, "") : resourcePaths[i];
+            String value2 = requestPaths[i];
+            if ((!value1.equals(value2) && !value1.equals(PATH_PARAM_IDENTIFIER)) || (value1.equals(PATH_PARAM_IDENTIFIER) && value2.equals("_history"))) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
